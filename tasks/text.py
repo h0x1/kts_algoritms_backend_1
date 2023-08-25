8e3da8c0eb5205059ca8ba0e6245ae4ddfb9a76c
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 17:06:36 +0300

    Implement find_shortest_longest_word

--- tasks/text.py
+++ tasks/text.py
@@ -4,10 +4,17 @@ __all__ = (
     'find_shortest_longest_word',
 )
 
-
 def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]:
-    """
-    В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
-    Если такого слова нет - вернуть None
-    """
-    raise NotImplementedError
+    minWord = "None"
+    maxWord = "None"
+    minLen = len(text)
+    maxLen = 0
+    splittedText = text.split()
+    for word in splittedText:
+        if len(word) < minLen:
+            minWord = word
+            minLen = len(word)
+        if len(word) > maxLen:
+            maxWord = word
+            maxLen = len(word)
+    return minWord, maxWord
