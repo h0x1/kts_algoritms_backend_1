92c49d8a2b68d2cfad40228c2895b03afa0dc9a7
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 19:20:45 +0300

    Update pairs.py

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -9,5 +9,5 @@ def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any
     newarr = []
     minLen = min(len(arr1),len(arr2))
     for i in range(0, minLen):
-        newarr.append([arr1[i],arr2[i]])
+        newarr.append((arr1[i],arr2[i]))
     return newarr
