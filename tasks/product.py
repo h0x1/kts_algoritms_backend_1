3eac806688ae24c67ef77169a84983c83ff108ab
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 19:20:58 +0300

    Update product.py

--- tasks/product.py
+++ tasks/product.py
@@ -10,5 +10,5 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     minLen = min(len(arr1),len(arr2))
     for i in range(0, minLen):
         for i2 in range(0, minLen):
-            newarr.append([arr1[i],arr2[i2]])
+            newarr.append((arr1[i],arr2[i2]))
     return newarr
