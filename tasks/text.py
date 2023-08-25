7ee6658d0d9049196581b97edfc232616d547541
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 17:19:38 +0300

    Update text.py

--- tasks/text.py
+++ tasks/text.py
@@ -5,8 +5,8 @@ __all__ = (
 )
 
 def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]:
-    minWord = "None"
-    maxWord = "None"
+    minWord = ""
+    maxWord = ""
     minLen = len(text)
     maxLen = 0
     splittedText = text.split()
@@ -17,4 +17,8 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
         if len(word) > maxLen:
             maxWord = word
             maxLen = len(word)
+    if not minWord:
+        minWord = None
+    if not maxWord:
+        maxWord = None
     return minWord, maxWord
