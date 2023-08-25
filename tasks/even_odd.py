bc60a3fb7cbc03168cfd896daf7c6e0e5d130143
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 19:13:24 +0300

    Update even_odd.py

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -6,6 +6,7 @@ __all__ = (
 def even_odd(arr: list[int]) -> float:
     chetSum = 0
     nechetSum = 0
+    i=0
     if len(arr) == 0:
         return 0
     for number in arr:
@@ -13,4 +14,6 @@ def even_odd(arr: list[int]) -> float:
             chetSum+=number
         else:
             nechetSum += number
+    if nechetSum == 0 or chetSum == 0:
+        return 0
     return(chetSum/nechetSum)
