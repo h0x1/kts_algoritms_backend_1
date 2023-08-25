589a760841627b5c4465918387c274b18543d709
h0x1 <37070924+h0x1@users.noreply.github.com>
2023-08-25 18:52:41 +0300

    implement corresponding_pairs

--- tasks/pairs.py
+++ tasks/pairs.py
@@ -6,8 +6,8 @@ __all__ = (
 
 
 def corresponding_pairs(arr1: list[Any], arr2: list[Any]) -> list[tuple[Any, Any]]:
-    """
-    Функция должна возвращать соответствующие элементы двух массивов:
-    corresponding_pairs([1, 2], [3, 4]) == [(1, 3), (2, 4)]
-    """
-    raise NotImplementedError
+    newarr = []
+    minLen = min(len(arr1),len(arr2))
+    for i in range(0, minLen):
+        newarr.append([arr1[i],arr2[i]])
+    return newarr
